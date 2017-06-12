88527c286f5c07a52f51db5f85442e609ccdc7b0
Antoinette Smith <antoinette@primeacademy.io>
2017-06-11 22:03:20 -0500

    test for li as generic by text did not work

--- tests/omega.js
+++ tests/omega.js
@@ -2,7 +2,7 @@ describe('Controller: Omega Controller', function () {
   it('displays a list of students on click of Display Students button', function () {
       browser.url('/');
       browser.click('button=Display Students');
-      expect(browser.getText('Moi')).to.exist;
-      expect(browser.getText('Kara')).to.exist;
+      expect(browser.getText('li=Moi')).to.exist;
+      expect(browser.getText('li=Kara')).to.exist;
     });
 });
