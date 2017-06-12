facde011cd7b0236a8d6184cb692a807b24c7a2c
Antoinette Smith <antoinette@primeacademy.io>
2017-06-12 10:04:45 -0500

    added starting condition test

--- tests/omega.js
+++ tests/omega.js
@@ -1,4 +1,13 @@
 describe('Controller: Omega Controller', function () {
+  it('does not display students on page load', function () {
+      browser.url('/');
+      try {
+        browser.getText('li=Kara'); // should throw error
+      } catch (e) {
+        expect(e).to.exist;
+      }
+    });
+
   it('displays a list of students on click of Display Students button', function () {
       browser.url('/');
       browser.click('button=Display Students');
