3ce814052416df1553dd9e3a1c8717678b5d01b7
Millie Walsh <millie@primeacademy.io>
2017-09-22 11:12:14 -0500

    change test dependancies on omega to canopus

--- tests/omega.js
+++ tests/omega.js
@@ -2,7 +2,7 @@ describe('Controller: Omega Controller', function () {
   it('does not display students on page load', function () {
       browser.url('/');
       try {
-        browser.getText('li=Kara'); // should throw error
+        browser.getText('li=Eric'); // should throw error
       } catch (e) {
         expect(e).to.exist;
       }
@@ -11,7 +11,7 @@ describe('Controller: Omega Controller', function () {
   it('displays a list of students on click of Display Students button', function () {
       browser.url('/');
       browser.click('button=Display Students');
-      expect(browser.getText('li=Moi')).to.exist;
-      expect(browser.getText('li=Kara')).to.exist;
+      expect(browser.getText('li=Eric')).to.exist;
+      expect(browser.getText('li=Kitty')).to.exist;
     });
 });
